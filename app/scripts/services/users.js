ae218fd2e55cf10f9b7a068b7d59239b8feb1830
Hadrien Lanneau <hadrien.lanneau@wikiogroup.com>
2014-05-05 11:30:11 +0200

    Header Nav Login form Logout Tests

--- app/scripts/services/users.js
+++ app/scripts/services/users.js
@@ -3,8 +3,13 @@
 angular.module('ngCraClientApp')
   .service('Users', function Users($http, $q) {
     function Users(config) {
+      this.uid = null;
+      this.username = '';
+      this.firstname = '';
+      this.lastname = '';
       _.extend(this, config);
     }
+
     Users.get = function() {
       var deferred = $q.defer(),
         ret = new Users();
@@ -17,12 +22,58 @@ angular.module('ngCraClientApp')
         _.extend(ret, data);
       })
       .error(function(err) {
-        console.log(err);
+        console.error(err);
       });
 
       ret.$promise = deferred.promise;
 
       return ret;
     };
+
+    Users.prototype.$login = function(username, password) {
+      var deferred = $q.defer(),
+        self = this;
+
+      $http({
+        method: 'post',
+        url: '/api/login',
+        data: {
+          username: username,
+          password: password
+        }
+      })
+      .success(function(data) {
+        deferred.resolve(_.extend(self, data));
+      })
+      .error(function(err) {
+        deferred.reject(err);
+      });
+
+      this.$promise = deferred.promise;
+
+      return this;
+    };
+
+    Users.prototype.$logout = function() {
+      var deferred = $q.defer(),
+        self = this;
+
+      $http({
+        method: 'get',
+        url: '/api/logout'
+      })
+      .success(function() {
+        _.extend(self, new Users());
+        deferred.resolve(self);
+      })
+      .error(function(err) {
+        deferred.reject(err);
+      });
+
+      this.$promise = deferred.promise;
+
+      return this;
+    };
+
     return Users;
   });
--- test/spec/services/users.js
+++ test/spec/services/users.js
@@ -1,18 +1,60 @@
 'use strict';
 
-describe('Service: Users', function () {
+ddescribe('Service: Users', function () {
 
   // load the service's module
   beforeEach(module('ngCraClientApp'));
 
   // instantiate service
-  var Users;
-  beforeEach(inject(function (_Users_) {
+  var Users,
+    $httpBackend;
+
+  beforeEach(inject(function (_Users_, _$httpBackend_) {
     Users = _Users_;
+    $httpBackend = _$httpBackend_;
   }));
 
-  it('should do something', function () {
-    expect(!!Users).toBe(true);
+  it('should get currently authentied user', function () {
+    $httpBackend.expectGET('/api/test')
+      .respond({uid: 1, username: 'hadrien'});
+
+    var user = Users.get();
+
+    $httpBackend.flush();
+
+    expect(user.uid).toBe(1);
+    expect(user.username).toBe('hadrien');
+  });
+
+  it('should login', function () {
+    $httpBackend.expectPOST('/api/login')
+      .respond({uid: 1, username: 'hadrien'});
+
+    var user = new Users();
+
+    user.$login('hadrien', '123456');
+
+    $httpBackend.flush();
+
+    expect(user.uid).toBe(1);
+    expect(user.username).toBe('hadrien');
+  });
+
+  it('should logout', function () {
+    $httpBackend.expectGET('/api/logout')
+      .respond(false);
+
+    var user = new Users({
+      uid: 1,
+      username: 'hadrien'
+    });
+
+    user.$logout();
+
+    $httpBackend.flush();
+
+    expect(user.uid).toBe(null);
+    expect(user.username).toBe('');
   });
 
 });
